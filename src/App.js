0e4155dd6809aebd5dc5eed2318dbbd0fc3f85c6
Rinni Vatsa <rinnivatsa09@gmail.com>
2024-09-24 21:42:28 +0530

    container Fix

--- src/App.js
+++ src/App.js
@@ -5,44 +5,7 @@ import BlogGridWithSidebar from './components/BlogGridWithSidebar';
 import blogs from './assets/blogs.json';
 import "./App.css"
 const App = () => {
-    // Blog post data
-    const blogData = [{
-            title: "Blog Post 1",
-            description: "This is a brief description of blog post 1.",
-            image: "path-to-image1.jpg",
-            date: "September 23, 2024",
-        },
-        {
-            title: "Blog Post 2",
-            description: "This is a brief description of blog post 2.",
-            image: "path-to-image2.jpg",
-            date: "September 22, 2024",
-        },
-        {
-            title: "Blog Post 3",
-            description: "This is a brief description of blog post 3.",
-            image: "path-to-image3.jpg",
-            date: "September 21, 2024",
-        },
-        {
-            title: "Blog Post 4",
-            description: "This is a brief description of blog post 4.",
-            image: "path-to-image4.jpg",
-            date: "September 20, 2024",
-        },
-        {
-            title: "Blog Post 5",
-            description: "This is a brief description of blog post 5.",
-            image: "path-to-image5.jpg",
-            date: "September 19, 2024",
-        },
-        {
-            title: "Blog Post 6",
-            description: "This is a brief description of blog post 6.",
-            image: "path-to-image6.jpg",
-            date: "September 18, 2024",
-        },
-    ];
+
     return ( <
         div >
         <
